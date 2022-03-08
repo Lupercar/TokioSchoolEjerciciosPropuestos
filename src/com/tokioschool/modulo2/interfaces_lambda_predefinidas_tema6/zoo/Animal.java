52c24ec24ad678ad5349a2bb895a5e1c2e7c64a7
Lupercar <hotolete@gmail.com>
2022-03-08 19:45:27 +0100

    Comienzo de proyecto

---
+++ src/com/tokioschool/modulo2/interfaces_lambda_predefinidas_tema6/zoo/Animal.java
@@ -0,0 +1,5 @@
+package com.tokioschool.modulo2.interfaces_lambda_predefinidas_tema6.zoo;
+
+public class Animal {
+
+}
---
+++ src/com/tokioschool/modulo2/interfaces_lambda_predefinidas_tema6/zoo/Main.java
@@ -0,0 +1,9 @@
+package com.tokioschool.modulo2.interfaces_lambda_predefinidas_tema6.zoo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Zoo");
+	}
+
+}
