8bbad2c062e09e2ef39adadf99e7bf6feb073ab3
Lupercar <hotolete@gmail.com>
2022-03-08 20:43:20 +0100

    Punto inicial ejercicio 2 tema 5

---
+++ src/com/tokioschool/modulo2/interfaz_funcional_tema5/funcionesl_ejercicio2/Main.java
@@ -0,0 +1,14 @@
+package com.tokioschool.modulo2.interfaz_funcional_tema5.funcionesl_ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Dogs y funciones");
+		Dog boi = new Dog("boi", 30, 6);
+		Dog tyri = new Dog("tyri", 40, 12);
+		Dog charis = new Dog("charis", 120, 7);
+
+		System.out.println(boi);
+	}
+
+}
