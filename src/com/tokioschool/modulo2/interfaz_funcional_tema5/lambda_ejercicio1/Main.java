98ffa2179a5a9016e88755fcafb92f4284521219
Lupercar <hotolete@gmail.com>
2022-03-09 13:48:37 +0100

    Create Main.java
    Punto inicial ejercicio1 tema5

---
+++ src/com/tokioschool/modulo2/interfaz_funcional_tema5/lambda_ejercicio1/Main.java
@@ -0,0 +1,16 @@
+package com.tokioschool.modulo2.interfaz_funcional_tema5.lambda_ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int [] arrayNumber = {0,1,2,3,4,5};
+		for (int number : arrayNumber) {
+			System.out.printf("\nEl factorial de %d es %d",number,factorial(number));
+		}
+	}
+
+	private static int factorial(int number) {
+		return 0;
+	}
+
+}
